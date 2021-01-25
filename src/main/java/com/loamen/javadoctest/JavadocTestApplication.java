e60110592b4bdf27b0d6343656238a2e6ec14f33
Don <donyang@163.com>
2021-01-25 21:58:21 +0800

    [update]添加测试代码注释

--- src/main/java/com/loamen/javadoctest/JavadocTestApplication.java
+++ src/main/java/com/loamen/javadoctest/JavadocTestApplication.java
@@ -3,6 +3,12 @@ package com.loamen.javadoctest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * javadoc测试应用程序
+ *
+ * @author Don
+ * @date 2021/01/25
+ */
 @SpringBootApplication
 public class JavadocTestApplication {
 
--- src/test/java/com/loamen/javadoctest/JavadocTestApplicationTests.java
+++ src/test/java/com/loamen/javadoctest/JavadocTestApplicationTests.java
@@ -3,9 +3,20 @@ package com.loamen.javadoctest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * javadoc测试应用程序测试
+ * The type Javadoc test application tests.
+ *
+ * @author Don
+ * @date 2021/01/25
+ */
 @SpringBootTest
 class JavadocTestApplicationTests {
 
+    /**
+     * 上下文加载
+     * Context loads.
+     */
     @Test
     void contextLoads() {
     }
