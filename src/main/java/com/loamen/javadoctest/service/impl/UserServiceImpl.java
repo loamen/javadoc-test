4195a95f5e13b8e9b8109e1e8045c290eed578f6
Don <donyang@163.com>
2021-03-16 17:59:19 +0800

    [update]modified description

--- src/main/java/com/loamen/javadoctest/service/impl/UserServiceImpl.java
+++ src/main/java/com/loamen/javadoctest/service/impl/UserServiceImpl.java
@@ -7,22 +7,46 @@ import com.loamen.javadoctest.service.UserService;
 /**
  * The type User service.
  *
- * @Author Administrator
- * @Description 一个苦逼的程序员
+ * @Author Don
+ * @Description Don
  * @Date 2021 /1/21 10:35
  */
 public class UserServiceImpl implements UserService {
+    /**
+     * 编号
+     */
+    public Integer id = 1;
+    /**
+     * 名称
+     */
+    private String name = "张三";
+    /**
+     * 年龄
+     */
+    private final int age = 24;
 
+    /**
+     * 获取用户.
+     *
+     * @param id the id
+     * @return the user
+     */
     @Override
     public User getUser(Integer id) {
-        User user = new User("张三");
-        user.id = 1;
+        User user = new User(name);
+        user.id = id;
         user.gender = Gender.FEMALE;
-        user.age = 22;
+        user.age = age;
 
         return user;
     }
 
+    /**
+     * 获取用户.
+     *
+     * @param name the name
+     * @return the user
+     */
     @Override
     public User getUser(String name) {
         User user = new User("李四");
